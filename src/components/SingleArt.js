4de255911756dbc4275940a04804832e9245aea7
kaif1baloch <117274273+kaif1baloch@users.noreply.github.com>
2024-05-25 08:50:35 +0500

    Update SingleArt.js

--- src/components/SingleArt.js
+++ src/components/SingleArt.js
@@ -105,100 +105,34 @@ const SingleArt = () => {
             <Button variant="primary" className="me-2 w-50 p-4 mt-4 btn-theme" style={{borderRadius: '50px'}} onClick={handleShow}>Purchase</Button>
         </Col>
     </Row>
-<Row className='m-0 px-4 py-5 bg-theme' style={{ background: 'rgb(255 255 255 / 80%)', overflowX: 'auto' }}>
-<Col xs= {6}>
-    <h2 style={{fontWeight: '600'}}>About the work</h2>
-    <p style={{fontSize: '15px'}}><strong>Materials:</strong> Giclee print on 300gsm Somerset velvet mould-made paper with hand made deckled edges</p>
-    <p style={{fontSize: '15px'}}><strong>Size:</strong> 11 4/5 × 8 2/5 in | 29.9 × 21.4 cm</p>
-    <p style={{fontSize: '15px'}}><strong>Rarity:</strong> <a href="#">Limited edition</a></p>
-    <p style={{fontSize: '15px'}}><strong>Medium:</strong> <a href="#">Print</a></p>
-    <p style={{fontSize: '15px'}}><strong>Condition:</strong> Mint Condition</p>
-    <p style={{fontSize: '15px'}}><strong>Signature:</strong> Hand-signed by artist, Stored flat inside the original Charleston portfolio with the accompanying hand signed and numbered COA. <a href="#">Read more</a></p>
-    <p style={{fontSize: '15px'}}><strong>Certificate of authenticity:</strong> Included (issued by gallery)</p>
-    <p style={{fontSize: '15px'}}><strong>Frame:</strong> Not included</p>
-</Col>
+<Row className='px-4 py-5 bg-theme' style={{ background: 'rgb(255 255 255 / 80%)', overflowX: 'auto' }}>
+  <Col xs= {6} className="px-4">
+      <h2 className="mb-3" style={{fontWeight: '600'}}>About the work</h2>
+      <p style={{fontSize: '15px'}}><strong>Materials:</strong> Giclee print on 300gsm Somerset velvet mould-made paper with hand made deckled edges</p>
+      <p style={{fontSize: '15px'}}><strong>Size:</strong> 11 4/5 × 8 2/5 in | 29.9 × 21.4 cm</p>
+      <p style={{fontSize: '15px'}}><strong>Rarity:</strong> <a href="#">Limited edition</a></p>
+      <p style={{fontSize: '15px'}}><strong>Medium:</strong> <a href="#">Print</a></p>
+      <p style={{fontSize: '15px'}}><strong>Condition:</strong> Mint Condition</p>
+      <p style={{fontSize: '15px'}}><strong>Signature:</strong> Hand-signed by artist, Stored flat inside the original Charleston portfolio with the accompanying hand signed and numbered COA. <a href="#">Read more</a></p>
+      <p style={{fontSize: '15px'}}><strong>Certificate of authenticity:</strong> Included (issued by gallery)</p>
+      <p style={{fontSize: '15px'}}><strong>Frame:</strong> Not included</p>
+  </Col>
+  <Col xs={6} className="px-4">
+    <h2 className="mb-3" style={{fontWeight: '600'}}>Tunji Adeniyi-Jones</h2>
+      <p>British, b. 1992</p>
+      <Button variant="primary" className="me-2">Follow</Button>
+      <p>With vibrant swirls of color and an eye towards West African aesthetics, Tunji Adeniyi-Jones paints Black bodies in repose and lush, mystical flora and fauna. The U.K.-born, New York-based artist draws on both his Yoruba heritage and diasporic experiences to inform his beguiling canvases, where he honors his ancestors’ ceremonies and celebrations. Adeniyi-Jones received his MFA from Yale and has exhibited in New York, Los Angeles, Stockholm, and London. His work regularly sells for six figures on the secondary market. Adeniyi-Jones believes that his portrayals of the body can transcend linguistic barriers and present a common language of movement and intimacy.</p>
+      <p><strong>High Auction Record:</strong> £302.4k, Phillips, 2021</p>
+      <p><strong>Blue Chip:</strong> Represented by internationally recognized galleries.</p>
+      <p><strong>Collected by a major museum</strong> Studio Museum in Harlem|ICA Miami</p>
+  </Col>
 </Row>
-<Row className='m-0 px-4 py-5 bg-theme' style={{ background: 'rgb(255 255 255 / 80%)', overflowX: 'auto' }}>
-<Col xs={6}>
-<h3>Tunji Adeniyi-Jones</h3>
-        <p>British, b. 1992</p>
-        <Button variant="primary" className="me-2">Follow</Button>
-        <p>With vibrant swirls of color and an eye towards West African aesthetics, Tunji Adeniyi-Jones paints Black bodies in repose and lush, mystical flora and fauna. The U.K.-born, New York-based artist draws on both his Yoruba heritage and diasporic experiences to inform his beguiling canvases, where he honors his ancestors’ ceremonies and celebrations. Adeniyi-Jones received his MFA from Yale and has exhibited in New York, Los Angeles, Stockholm, and London. His work regularly sells for six figures on the secondary market. Adeniyi-Jones believes that his portrayals of the body can transcend linguistic barriers and present a common language of movement and intimacy.</p>
-        <p><strong>High Auction Record:</strong> £302.4k, Phillips, 2021</p>
-        <p><strong>Blue Chip:</strong> Represented by internationally recognized galleries.</p>
-        <p><strong>Collected by a major museum</strong> Studio Museum in Harlem|ICA Miami</p>
-        <h3>Selected Exhibitions</h3>
-        <p><strong>2024:</strong> Connections: Wingate Studio at David Krut Projects, David Krut Projects</p>
-        <p><strong>2021:</strong> Melodic Virtues, Morán Morán <br>
-        </br>That Which Binds Us, White Cube</p> 
-</Col>
-</Row>
-<Row  className='m-0 px-4 py-5 bg-theme' style={{ background: 'rgb(255 255 255 / 80%)', overflowX: 'auto' }}>
+<Row  className='m-0 px-4 py-3 bg-theme' style={{ background: 'rgb(255 255 255 / 80%)', overflowX: 'auto' }}>
     <Col xs= {12}>
     <h2>Other works by Tunji Adeniyi-Jones</h2>
     </Col>
 </Row>
-<Row  className='m-0 px-4 py-5 bg-theme' style={{ background: 'rgb(255 255 255 / 80%)', overflowX: 'auto' }}>
-  <Col xs= {3} className="d-flex flex-column gap-1">
-    <img src="https://img.freepik.com/free-photo/colorful-design-with-spiral-design_188544-9588.jpg?t=st=1716602028~exp=1716605628~hmac=066235c94a67c677be55bc0ecb0c8eac42ebcfe882b7c5681d8dc519c9abcb69&w=996" alt="Artwork by Tunji Adeniyi-Jones" className="img-fluid rounded" /> 
-    <p className="m-0">Tunji Adeniyi-Jones</p>
-    <p className="m-0"><em>Poetic Feet</em>, 2022</p>
-    <p className="m-0">New Art Editions</p>
-    <p className="m-0">€795</p>
-  </Col>
-  <Col xs= {3} className="d-flex flex-column gap-1">
-    <img src="https://img.freepik.com/free-photo/colorful-design-with-spiral-design_188544-9588.jpg?t=st=1716602028~exp=1716605628~hmac=066235c94a67c677be55bc0ecb0c8eac42ebcfe882b7c5681d8dc519c9abcb69&w=996" alt="Artwork by Tunji Adeniyi-Jones" className="img-fluid rounded" /> 
-    <p className="m-0">Tunji Adeniyi-Jones</p>
-    <p className="m-0"><em>Poetic Feet</em>, 2022</p>
-    <p className="m-0">New Art Editions</p>
-    <p className="m-0">€795</p>
-  </Col>
-  <Col xs= {3} className="d-flex flex-column gap-1">
-    <img src="https://img.freepik.com/free-photo/colorful-design-with-spiral-design_188544-9588.jpg?t=st=1716602028~exp=1716605628~hmac=066235c94a67c677be55bc0ecb0c8eac42ebcfe882b7c5681d8dc519c9abcb69&w=996" alt="Artwork by Tunji Adeniyi-Jones" className="img-fluid rounded" /> 
-    <p className="m-0">Tunji Adeniyi-Jones</p>
-    <p className="m-0"><em>Poetic Feet</em>, 2022</p>
-    <p className="m-0">New Art Editions</p>
-    <p className="m-0">€795</p>
-  </Col>
-  <Col xs= {3} className="d-flex flex-column gap-1">
-    <img src="https://img.freepik.com/free-photo/colorful-design-with-spiral-design_188544-9588.jpg?t=st=1716602028~exp=1716605628~hmac=066235c94a67c677be55bc0ecb0c8eac42ebcfe882b7c5681d8dc519c9abcb69&w=996" alt="Artwork by Tunji Adeniyi-Jones" className="img-fluid rounded" /> 
-    <p className="m-0">Tunji Adeniyi-Jones</p>
-    <p className="m-0"><em>Poetic Feet</em>, 2022</p>
-    <p className="m-0">New Art Editions</p>
-    <p className="m-0">€795</p>
-  </Col>
-</Row>
-<Row  className='m-0 px-4 py-5 bg-theme' style={{ background: 'rgb(255 255 255 / 80%)', overflowX: 'auto' }}>
-  <Col xs= {3} className="d-flex flex-column gap-1">
-    <img src="https://img.freepik.com/free-photo/colorful-design-with-spiral-design_188544-9588.jpg?t=st=1716602028~exp=1716605628~hmac=066235c94a67c677be55bc0ecb0c8eac42ebcfe882b7c5681d8dc519c9abcb69&w=996" alt="Artwork by Tunji Adeniyi-Jones" className="img-fluid rounded" /> 
-    <p className="m-0">Tunji Adeniyi-Jones</p>
-    <p className="m-0"><em>Poetic Feet</em>, 2022</p>
-    <p className="m-0">New Art Editions</p>
-    <p className="m-0">€795</p>
-  </Col>
-  <Col xs= {3} className="d-flex flex-column gap-1">
-    <img src="https://img.freepik.com/free-photo/colorful-design-with-spiral-design_188544-9588.jpg?t=st=1716602028~exp=1716605628~hmac=066235c94a67c677be55bc0ecb0c8eac42ebcfe882b7c5681d8dc519c9abcb69&w=996" alt="Artwork by Tunji Adeniyi-Jones" className="img-fluid rounded" /> 
-    <p className="m-0">Tunji Adeniyi-Jones</p>
-    <p className="m-0"><em>Poetic Feet</em>, 2022</p>
-    <p className="m-0">New Art Editions</p>
-    <p className="m-0">€795</p>
-  </Col>
-  <Col xs= {3} className="d-flex flex-column gap-1">
-    <img src="https://img.freepik.com/free-photo/colorful-design-with-spiral-design_188544-9588.jpg?t=st=1716602028~exp=1716605628~hmac=066235c94a67c677be55bc0ecb0c8eac42ebcfe882b7c5681d8dc519c9abcb69&w=996" alt="Artwork by Tunji Adeniyi-Jones" className="img-fluid rounded" /> 
-    <p className="m-0">Tunji Adeniyi-Jones</p>
-    <p className="m-0"><em>Poetic Feet</em>, 2022</p>
-    <p className="m-0">New Art Editions</p>
-    <p className="m-0">€795</p>
-  </Col>
-  <Col xs= {3} className="d-flex flex-column gap-1">
-    <img src="https://img.freepik.com/free-photo/colorful-design-with-spiral-design_188544-9588.jpg?t=st=1716602028~exp=1716605628~hmac=066235c94a67c677be55bc0ecb0c8eac42ebcfe882b7c5681d8dc519c9abcb69&w=996" alt="Artwork by Tunji Adeniyi-Jones" className="img-fluid rounded" /> 
-    <p className="m-0">Tunji Adeniyi-Jones</p>
-    <p className="m-0"><em>Poetic Feet</em>, 2022</p>
-    <p className="m-0">New Art Editions</p>
-    <p className="m-0">€795</p>
-  </Col>
-</Row>
-<Row  className='m-0 px-4 py-5 bg-theme' style={{ background: 'rgb(255 255 255 / 80%)', overflowX: 'auto' }}>
+<Row  className='m-0 px-4 pb-5 bg-theme' style={{ background: 'rgb(255 255 255 / 80%)', overflowX: 'auto' }}>
   <Col xs= {3} className="d-flex flex-column gap-1">
     <img src="https://img.freepik.com/free-photo/colorful-design-with-spiral-design_188544-9588.jpg?t=st=1716602028~exp=1716605628~hmac=066235c94a67c677be55bc0ecb0c8eac42ebcfe882b7c5681d8dc519c9abcb69&w=996" alt="Artwork by Tunji Adeniyi-Jones" className="img-fluid rounded" /> 
     <p className="m-0">Tunji Adeniyi-Jones</p>
