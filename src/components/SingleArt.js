08e92cd16378407741cefc209cf44fffac4314c7
Insia2001 <63118154+Insia2001@users.noreply.github.com>
2024-05-25 08:37:58 +0500

    Update SingleArt.js

--- src/components/SingleArt.js
+++ src/components/SingleArt.js
@@ -39,7 +39,147 @@ const SingleArt = () => {
   <p><strong>Frame:</strong> Not included</p>
 </Col>
 </Row>
-   
+<Row className='m-0 px-4 py-5 bg-theme' style={{ background: 'rgb(255 255 255 / 80%)', overflowX: 'auto' }}>
+<Col xs={6}>
+<h3>Tunji Adeniyi-Jones</h3>
+        <p>British, b. 1992</p>
+        <Button variant="primary" className="me-2">Follow</Button>
+        <p>With vibrant swirls of color and an eye towards West African aesthetics, Tunji Adeniyi-Jones paints Black bodies in repose and lush, mystical flora and fauna. The U.K.-born, New York-based artist draws on both his Yoruba heritage and diasporic experiences to inform his beguiling canvases, where he honors his ancestors’ ceremonies and celebrations. Adeniyi-Jones received his MFA from Yale and has exhibited in New York, Los Angeles, Stockholm, and London. His work regularly sells for six figures on the secondary market. Adeniyi-Jones believes that his portrayals of the body can transcend linguistic barriers and present a common language of movement and intimacy.</p>
+        <p><strong>High Auction Record:</strong> £302.4k, Phillips, 2021</p>
+        <p><strong>Blue Chip:</strong> Represented by internationally recognized galleries.</p>
+        <p><strong>Collected by a major museum</strong> Studio Museum in Harlem|ICA Miami</p>
+        <h3>Selected Exhibitions</h3>
+        <p><strong>2024:</strong> Connections: Wingate Studio at David Krut Projects, David Krut Projects</p>
+        <p><strong>2021:</strong> Melodic Virtues, Morán Morán <br>
+        </br>That Which Binds Us, White Cube</p> 
+</Col>
+</Row>
+<Row  className='m-0 px-4 py-5 bg-theme' style={{ background: 'rgb(255 255 255 / 80%)', overflowX: 'auto' }}>
+    <Col xs= {12}>
+    <h2>Other works by Tunji Adeniyi-Jones</h2>
+    </Col>
+</Row>
+<Row  className='m-0 px-4 py-5 bg-theme' style={{ background: 'rgb(255 255 255 / 80%)', overflowX: 'auto' }}>
+<Col xs= {3} className="d-flex flex-column gap-2">
+<img src="https://img.freepik.com/free-photo/colorful-design-with-spiral-design_188544-9588.jpg?t=st=1716602028~exp=1716605628~hmac=066235c94a67c677be55bc0ecb0c8eac42ebcfe882b7c5681d8dc519c9abcb69&w=996" alt="Artwork by Tunji Adeniyi-Jones" className="img-fluid rounded" /> 
+<p>Tunji Adeniyi-Jones</p>
+    <p><em>Poetic Feet</em>, 2022</p>
+    <p>New Art Editions</p>
+    <p>€795</p>
+</Col>
+<Col xs= {3}>
+<img src="https://img.freepik.com/free-photo/colorful-design-with-spiral-design_188544-9588.jpg?t=st=1716602028~exp=1716605628~hmac=066235c94a67c677be55bc0ecb0c8eac42ebcfe882b7c5681d8dc519c9abcb69&w=996" alt="Artwork by Tunji Adeniyi-Jones" className="img-fluid rounded" /> 
+<p>Tunji Adeniyi-Jones</p>
+    <p><em>Poetic Feet</em>, 2022</p>
+    <p>New Art Editions</p>
+    <p>€795</p>
+</Col>
+<Col xs= {3}>
+<img src="https://img.freepik.com/free-photo/colorful-design-with-spiral-design_188544-9588.jpg?t=st=1716602028~exp=1716605628~hmac=066235c94a67c677be55bc0ecb0c8eac42ebcfe882b7c5681d8dc519c9abcb69&w=996" alt="Artwork by Tunji Adeniyi-Jones" className="img-fluid rounded" /> 
+<p>Tunji Adeniyi-Jones</p>
+    <p><em>Poetic Feet</em>, 2022</p>
+    <p>New Art Editions</p>
+    <p>€795</p>
+</Col>
+<Col xs= {3}>
+<img src="https://img.freepik.com/free-photo/colorful-design-with-spiral-design_188544-9588.jpg?t=st=1716602028~exp=1716605628~hmac=066235c94a67c677be55bc0ecb0c8eac42ebcfe882b7c5681d8dc519c9abcb69&w=996" alt="Artwork by Tunji Adeniyi-Jones" className="img-fluid rounded" /> 
+<p>Tunji Adeniyi-Jones</p>
+    <p><em>Poetic Feet</em>, 2022</p>
+    <p>New Art Editions</p>
+    <p>€795</p>
+</Col>
+</Row>
+<Row  className='m-0 px-4 py-5 bg-theme' style={{ background: 'rgb(255 255 255 / 80%)', overflowX: 'auto' }}>
+<Col xs= {3}>
+<img src="https://img.freepik.com/free-photo/colorful-design-with-spiral-design_188544-9588.jpg?t=st=1716602028~exp=1716605628~hmac=066235c94a67c677be55bc0ecb0c8eac42ebcfe882b7c5681d8dc519c9abcb69&w=996" alt="Artwork by Tunji Adeniyi-Jones" className="img-fluid rounded" /> 
+<p>Tunji Adeniyi-Jones</p>
+    <p><em>Poetic Feet</em>, 2022</p>
+    <p>New Art Editions</p>
+    <p>€795</p>
+</Col>
+<Col xs= {3}>
+<img src="https://img.freepik.com/free-photo/colorful-design-with-spiral-design_188544-9588.jpg?t=st=1716602028~exp=1716605628~hmac=066235c94a67c677be55bc0ecb0c8eac42ebcfe882b7c5681d8dc519c9abcb69&w=996" alt="Artwork by Tunji Adeniyi-Jones" className="img-fluid rounded" /> 
+<p>Tunji Adeniyi-Jones</p>
+    <p><em>Poetic Feet</em>, 2022</p>
+    <p>New Art Editions</p>
+    <p>€795</p>
+</Col>
+<Col xs= {3}>
+<img src="https://img.freepik.com/free-photo/colorful-design-with-spiral-design_188544-9588.jpg?t=st=1716602028~exp=1716605628~hmac=066235c94a67c677be55bc0ecb0c8eac42ebcfe882b7c5681d8dc519c9abcb69&w=996" alt="Artwork by Tunji Adeniyi-Jones" className="img-fluid rounded" /> 
+<p>Tunji Adeniyi-Jones</p>
+    <p><em>Poetic Feet</em>, 2022</p>
+    <p>New Art Editions</p>
+    <p>€795</p>
+</Col>
+<Col xs= {3}>
+<img src="https://img.freepik.com/free-photo/colorful-design-with-spiral-design_188544-9588.jpg?t=st=1716602028~exp=1716605628~hmac=066235c94a67c677be55bc0ecb0c8eac42ebcfe882b7c5681d8dc519c9abcb69&w=996" alt="Artwork by Tunji Adeniyi-Jones" className="img-fluid rounded" /> 
+<p>Tunji Adeniyi-Jones</p>
+    <p><em>Poetic Feet</em>, 2022</p>
+    <p>New Art Editions</p>
+    <p>€795</p>
+</Col>
+</Row>
+<Row  className='m-0 px-4 py-5 bg-theme' style={{ background: 'rgb(255 255 255 / 80%)', overflowX: 'auto' }}>
+<Col xs= {3}>
+<img src="https://img.freepik.com/free-photo/colorful-design-with-spiral-design_188544-9588.jpg?t=st=1716602028~exp=1716605628~hmac=066235c94a67c677be55bc0ecb0c8eac42ebcfe882b7c5681d8dc519c9abcb69&w=996" alt="Artwork by Tunji Adeniyi-Jones" className="img-fluid rounded" /> 
+<p>Tunji Adeniyi-Jones</p>
+    <p><em>Poetic Feet</em>, 2022</p>
+    <p>New Art Editions</p>
+    <p>€795</p>
+</Col>
+<Col xs= {3}>
+<img src="https://img.freepik.com/free-photo/colorful-design-with-spiral-design_188544-9588.jpg?t=st=1716602028~exp=1716605628~hmac=066235c94a67c677be55bc0ecb0c8eac42ebcfe882b7c5681d8dc519c9abcb69&w=996" alt="Artwork by Tunji Adeniyi-Jones" className="img-fluid rounded" /> 
+<p>Tunji Adeniyi-Jones</p>
+    <p><em>Poetic Feet</em>, 2022</p>
+    <p>New Art Editions</p>
+    <p>€795</p>
+</Col>
+<Col xs= {3}>
+<img src="https://img.freepik.com/free-photo/colorful-design-with-spiral-design_188544-9588.jpg?t=st=1716602028~exp=1716605628~hmac=066235c94a67c677be55bc0ecb0c8eac42ebcfe882b7c5681d8dc519c9abcb69&w=996" alt="Artwork by Tunji Adeniyi-Jones" className="img-fluid rounded" /> 
+<p>Tunji Adeniyi-Jones</p>
+    <p><em>Poetic Feet</em>, 2022</p>
+    <p>New Art Editions</p>
+    <p>€795</p>
+</Col>
+<Col xs= {3}>
+<img src="https://img.freepik.com/free-photo/colorful-design-with-spiral-design_188544-9588.jpg?t=st=1716602028~exp=1716605628~hmac=066235c94a67c677be55bc0ecb0c8eac42ebcfe882b7c5681d8dc519c9abcb69&w=996" alt="Artwork by Tunji Adeniyi-Jones" className="img-fluid rounded" /> 
+<p>Tunji Adeniyi-Jones</p>
+    <p><em>Poetic Feet</em>, 2022</p>
+    <p>New Art Editions</p>
+    <p>€795</p>
+</Col>
+</Row>
+<Row  className='m-0 px-4 py-5 bg-theme' style={{ background: 'rgb(255 255 255 / 80%)', overflowX: 'auto' }}>
+<Col xs= {3} >
+<img src="https://img.freepik.com/free-photo/colorful-design-with-spiral-design_188544-9588.jpg?t=st=1716602028~exp=1716605628~hmac=066235c94a67c677be55bc0ecb0c8eac42ebcfe882b7c5681d8dc519c9abcb69&w=996" alt="Artwork by Tunji Adeniyi-Jones" className="img-fluid rounded" /> 
+<p>Tunji Adeniyi-Jones</p>
+    <p><em>Poetic Feet</em>, 2022</p>
+    <p>New Art Editions</p>
+    <p>€795</p>
+</Col>
+<Col xs= {3}>
+<img src="https://img.freepik.com/free-photo/colorful-design-with-spiral-design_188544-9588.jpg?t=st=1716602028~exp=1716605628~hmac=066235c94a67c677be55bc0ecb0c8eac42ebcfe882b7c5681d8dc519c9abcb69&w=996" alt="Artwork by Tunji Adeniyi-Jones" className="img-fluid rounded" /> 
+<p>Tunji Adeniyi-Jones</p>
+    <p><em>Poetic Feet</em>, 2022</p>
+    <p>New Art Editions</p>
+    <p>€795</p>
+</Col>
+<Col xs= {3}>
+<img src="https://img.freepik.com/free-photo/colorful-design-with-spiral-design_188544-9588.jpg?t=st=1716602028~exp=1716605628~hmac=066235c94a67c677be55bc0ecb0c8eac42ebcfe882b7c5681d8dc519c9abcb69&w=996" alt="Artwork by Tunji Adeniyi-Jones" className="img-fluid rounded" /> 
+<p>Tunji Adeniyi-Jones</p>
+    <p><em>Poetic Feet</em>, 2022</p>
+    <p>New Art Editions</p>
+    <p>€795</p>
+</Col>
+<Col xs= {3}>
+<img src="https://img.freepik.com/free-photo/colorful-design-with-spiral-design_188544-9588.jpg?t=st=1716602028~exp=1716605628~hmac=066235c94a67c677be55bc0ecb0c8eac42ebcfe882b7c5681d8dc519c9abcb69&w=996" alt="Artwork by Tunji Adeniyi-Jones" className="img-fluid rounded" /> 
+<p>Tunji Adeniyi-Jones</p>
+    <p><em>Poetic Feet</em>, 2022</p>
+    <p>New Art Editions</p>
+    <p>€795</p>
+</Col>
+</Row>
+
   </>
   )
 }
